81ca2a0b4b7006d5c5ab7febcc4782f96cee14a4
vsbhavaraju <vsbhavaraju@gmail.com>
2018-03-05 10:19:07 -0700

    Logging lab

--- src/main/java/org/superbiz/moviefun/Application.java
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -1,38 +1,39 @@
 package org.superbiz.moviefun;
 
+
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+
 import org.superbiz.moviefun.blobstore.BlobStore;
 import org.superbiz.moviefun.blobstore.S3Store;
 import org.superbiz.moviefun.blobstore.ServiceCredentials;
 
+
 @SpringBootApplication
 public class Application {
+
 
     public static void main(String... args) {
         SpringApplication.run(Application.class, args);
     }
 
     @Bean
+
     public ServletRegistrationBean actionServletRegistration(ActionServlet actionServlet) {
         return new ServletRegistrationBean(actionServlet, "/moviefun/*");
     }
 
     @Bean
-    ServiceCredentials serviceCredentials(@Value("${vcap.services}") String vcapServices) {
-        return new ServiceCredentials(vcapServices);
-    }
-
-    @Bean
     public BlobStore blobStore(
-        ServiceCredentials serviceCredentials,
-        @Value("${s3.endpointUrl:#{null}}") String s3EndpointUrl
+            ServiceCredentials serviceCredentials,
+            @Value("${s3.endpointUrl:#{null}}") String s3EndpointUrl
     ) {
         String s3AccessKey = serviceCredentials.getCredential("moviefun-s3", "aws-s3", "access_key_id");
         String s3SecretKey = serviceCredentials.getCredential("moviefun-s3", "aws-s3", "secret_access_key");
@@ -47,4 +48,11 @@ public class Application {
 
         return new S3Store(s3Client, s3BucketName);
     }
+
+    @Bean
+    ServiceCredentials serviceCredentials(@Value("${vcap.services}") String vcapServices) {
+        return new ServiceCredentials(vcapServices);
+    }
+
+
 }
--- src/main/java/org/superbiz/moviefun/movies/MoviesBean.java
+++ src/main/java/org/superbiz/moviefun/movies/MoviesBean.java
@@ -16,6 +16,8 @@
  */
 package org.superbiz.moviefun.movies;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -38,8 +40,7 @@ public class MoviesBean {
 
     @Transactional
     public void addMovie(Movie movie) {
-        System.err.println("Creating movie with title " + movie.getTitle() +
-            ", and year " + movie.getYear());
+        logger.debug("Creating movie with title {} and year {}", movie.getTitle(), movie.getYear());
 
         entityManager.persist(movie);
     }
@@ -117,4 +118,6 @@ public class MoviesBean {
     public void clean() {
         entityManager.createQuery("delete from Movie").executeUpdate();
     }
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 }
